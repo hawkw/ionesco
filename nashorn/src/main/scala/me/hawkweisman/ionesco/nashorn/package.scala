539da70b310329993cdc57b7f1eafd4f5c985eee
Hawk Weisman <hi@hawkweisman.me>
2016-07-27 15:12:06 -0400

    nashorn: import feature flag for implicits

--- nashorn/src/main/scala/me/hawkweisman/ionesco/nashorn/package.scala
+++ nashorn/src/main/scala/me/hawkweisman/ionesco/nashorn/package.scala
@@ -2,11 +2,13 @@ package me.hawkweisman.ionesco
 
 import jdk.nashorn.api.scripting.ScriptObjectMirror
 
+import scala.language.implicitConversions
+
 /**
   * Created by Eliza on 7/27/16.
   */
 package object nashorn {
 
   implicit def wrapScriptObject(obj: ScriptObjectMirror): IonescoNashornObject
-  = new IonescoNashornObject(obj)
+    = new IonescoNashornObject(obj)
 }
