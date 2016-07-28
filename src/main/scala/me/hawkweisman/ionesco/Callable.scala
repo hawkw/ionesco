5d6bbecfb17dbc2a36b4bb7dbb749e03b5455a8c
Hawk Weisman <hi@hawkweisman.me>
2016-07-28 10:36:12 -0400

    core: make Callable.apply() return Try

--- src/main/scala/me/hawkweisman/ionesco/Callable.scala
+++ src/main/scala/me/hawkweisman/ionesco/Callable.scala
@@ -1,9 +1,11 @@
 package me.hawkweisman.ionesco
 
+import scala.util.Try
+
 /** Trait for a JavaScript value that's callable (e.g. a function).
   *
   * Created by Eliza on 7/28/16.
   */
 trait Callable {
-  def apply(args: AnyRef*): AnyRef
+  def apply(args: AnyRef*): Try[AnyRef]
 }
