a8db327dc22ceccbc58c205c943b3f1fde72be25
Hawk Weisman <hi@hawkweisman.me>
2016-07-28 11:27:36 -0400

    core: move indexing to JsArray

--- src/main/scala/me/hawkweisman/ionesco/Indexable.scala
+++ src/main/scala/me/hawkweisman/ionesco/Indexable.scala
@@ -7,27 +7,6 @@ import scala.util.Try
   */
 trait Indexable {
 
-  def length: Int
-
-  /** Attempt to index the JS array.
-    *
-    * @inheritdoc
-    *
-    * The returned [[Index]] can then be resolved to the desired type
-    *
-    * @param idx the index to access
-    * @return    an [[Index]] object representing the indexing attempt
-    * @throws    IndexOutOfBoundsException if `i < 0` or `length <= i`
-    */
-  @inline def apply(idx: Int): Index
-    = idx match {
-      case i if idx < 0 =>
-        throw new IndexOutOfBoundsException(s"index $i < 0")
-      case i if length <= idx =>
-        throw new IndexOutOfBoundsException(s"index $i >= length ($length)")
-      case i => new Index(i, this)
-    }
-
   protected def rawIndex(idx: Int): Try[AnyRef]
   protected def rawIndexOption(idx: Int): Option[AnyRef]
 
--- src/main/scala/me/hawkweisman/ionesco/JsArray.scala
+++ src/main/scala/me/hawkweisman/ionesco/JsArray.scala
@@ -7,4 +7,24 @@ trait JsArray
 extends Indexable
   with IndexedSeq[Resolvable] {
 
+
+  /** Attempt to index the JS array.
+    *
+    * @inheritdoc
+    *
+    * The returned [[Index]] can then be resolved to the desired type
+    *
+    * @param idx the index to access
+    * @return    an [[Index]] object representing the indexing attempt
+    * @throws    IndexOutOfBoundsException if `i < 0` or `length <= i`
+    */
+  @inline def apply(idx: Int): Index
+    = idx match {
+      case i if idx < 0 =>
+        throw new IndexOutOfBoundsException(s"index $i < 0")
+      case i if length <= idx =>
+        throw new IndexOutOfBoundsException(s"index $i >= length ($length)")
+      case i => new Index(i, this)
+    }
+
 }
