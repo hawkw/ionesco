2d5a4db1a67d003226711dd9a67c197703d63872
Hawk Weisman <hi@hawkweisman.me>
2016-07-28 11:28:00 -0400

    json: add tests for JSON arrays

--- json/src/test/scala/me/hawkweisman/ionesco/json/test/AsSpec.scala
+++ json/src/test/scala/me/hawkweisman/ionesco/json/test/AsSpec.scala
@@ -2,7 +2,7 @@ package me.hawkweisman.ionesco
 package json
 package test
 
-import org.json.JSONObject
+import org.json.{JSONArray, JSONObject}
 import org.scalatest.{Matchers, OptionValues, TryValues, WordSpec}
 
 import scala.util.Success
@@ -80,74 +80,76 @@ class AsSpec
     }
   }
 
-//  "A JSON array" when {
-//    val array = new JSONArray("""[1, 2.0, "hi", {"whatever":"yes"}, true]""")
-//    "indexed to a position that exists" should {
-//      "extract an integer element as an Int" in {
-//        array(0).as[Int].success.value shouldEqual 1
-//      }
-//      "extract a decimal element as a Double" in {
-//        array(1).as[Double].success.value shouldEqual 2.0
-//      }
-//      "extract a string element as a String" in {
-//        array(2).as[String].success.value shouldEqual "hi"
-//      }
-//      "extract an object element as a JSONObject" in {
-//        array(3).as[JSONObject].success.value shouldBe a [JSONObject]
-//      }
-//      "allow child objects to be queried correctly" in {
-//        array(3).whatever.as[String].success.value shouldEqual "yes"
-//      }
-//      "extract a boolean element as a Boolean" in {
-//        array(4).as[Boolean].success.value shouldEqual true
-//      }
-//    }
-//    "indexed to a position equal to its length" should {
-//      "throw an exception" in {
-//        the [IndexOutOfBoundsException] thrownBy {
-//          array(5)
-//        } should have message "index 5 >= length (5)"
-//      }
-//    }
-//    "indexed to a position greater than its length" should {
-//      "throw an exception" in {
-//        the [IndexOutOfBoundsException] thrownBy {
-//          array(6)
-//        } should have message "index 6 >= length (5)"
-//      }
-//    }
-//    "indexed to a position less than 0" should {
-//      "throw an exception" in {
-//        the [IndexOutOfBoundsException] thrownBy {
-//          array(-1)
-//        } should have message "index -1 < 0"
-//      }
-//    }
-//    "iterated over" should {
-//      val inOrderArray = new JSONArray("""[1,2,3,4,5,6,7,8,9,10]""")
-//      "contain the correct elements in the correct sequence" in {
-//        var i = 0
-//        inOrderArray foreach { idx =>
-//          i += 1
-//          idx.as[Int].success.value shouldEqual i
-//        }
-//      }
-//      "be useable in a for expression generator" in {
-//        var sum = 0
-//        for { idx <- inOrderArray
-//          value <- idx.as[Int] }
-//        { sum += value }
-//        sum shouldEqual 55
-//      }
-//      "be useable in a for-yield expression" in {
-//        val result
-//        = for { idx <- inOrderArray }
-//          yield { idx.as[Int].success.value + 1 }
-//
-//        result.toStream should contain inOrder (2,3,4,5,6,7,8,9,10,11)
-//      }
-//    }
-//  }
+  "A JSON array" when {
+    val array: JsArray = new JSONArray(
+      """[1, 2.0, "hi", {"whatever":"yes"},
+        |true]""".stripMargin)
+    "indexed to a position that exists" should {
+      "extract an integer element as an Int" in {
+        array(0).as[Int].success.value shouldEqual 1
+      }
+      "extract a decimal element as a Double" in {
+        array(1).as[Double].success.value shouldEqual 2.0
+      }
+      "extract a string element as a String" in {
+        array(2).as[String].success.value shouldEqual "hi"
+      }
+      "extract an object element as a JSONObject" in {
+        array(3).as[JsObject].success.value shouldBe a [JsObject]
+      }
+      "allow child objects to be queried correctly" in {
+        array(3).whatever.as[String].success.value shouldEqual "yes"
+      }
+      "extract a boolean element as a Boolean" in {
+        array(4).as[Boolean].success.value shouldEqual true
+      }
+    }
+    "indexed to a position equal to its length" should {
+      "throw an exception" in {
+        the [IndexOutOfBoundsException] thrownBy {
+          array(5)
+        } should have message "index 5 >= length (5)"
+      }
+    }
+    "indexed to a position greater than its length" should {
+      "throw an exception" in {
+        the [IndexOutOfBoundsException] thrownBy {
+          array(6)
+        } should have message "index 6 >= length (5)"
+      }
+    }
+    "indexed to a position less than 0" should {
+      "throw an exception" in {
+        the [IndexOutOfBoundsException] thrownBy {
+          array(-1)
+        } should have message "index -1 < 0"
+      }
+    }
+    "iterated over" should {
+      val inOrderArray: JsArray = new JSONArray("""[1,2,3,4,5,6,7,8,9,10]""")
+      "contain the correct elements in the correct sequence" in {
+        var i = 0
+        inOrderArray foreach { idx =>
+          i += 1
+          idx.as[Int].success.value shouldEqual i
+        }
+      }
+      "be useable in a for expression generator" in {
+        var sum = 0
+        for { idx <- inOrderArray
+          value <- idx.as[Int] }
+        { sum += value }
+        sum shouldEqual 55
+      }
+      "be useable in a for-yield expression" in {
+        val result
+        = for { idx <- inOrderArray }
+          yield { idx.as[Int].success.value + 1 }
+
+        result.toStream should contain inOrder (2,3,4,5,6,7,8,9,10,11)
+      }
+    }
+  }
 
 
 }
